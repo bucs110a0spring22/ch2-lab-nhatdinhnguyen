b9e643036b9faef6c09b750cc84497f2368bc8cb
Nhat-Dinh Nguyen Nguyen <98847679+nhatdinhnguyen@users.noreply.github.com>
2022-02-11 02:56:09 +0000

    codes!

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import random
 
 #Part A
 weeks = 16
+print("Weeks:", weeks)
 classes = 5
+print("Classes:", classes)
 tuition = 6000
+print("Tuition:", tuition)
 cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
-
+print("Cost per week:",cost_per_week)
+# 2 MWF classes and 3 TR classes
+classes_per_week = 12
+cost_per_class = (cost_per_week / classes_per_week)
+print("Cost per class:", cost_per_class)
 
 #Part B
+my_list = [1,2,3,4,5,6,7,8,9,10]
+years = random.choice(my_list)
+print("Years:", years)
